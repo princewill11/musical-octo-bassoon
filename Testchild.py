d36285248f943f8406899ab39be30f80ec44427d
princewill11 <59522847+princewill11@users.noreply.github.com>
2023-08-03 23:26:26 +0100

    Create Testchild.py
    A simple Javascript program

---
+++ Testchild.py
@@ -0,0 +1,12 @@
+// Function to calculate the sum of two numbers
+function calculateSum(a, b) {
+  return a + b;
+}
+
+// Sample numbers
+const num1 = 5;
+const num2 = 10;
+
+// Calculate the sum and print the result
+const sum = calculateSum(num1, num2);
+console.log("The sum is:", sum);
